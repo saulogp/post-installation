5cf2cfc798e124e5bc6040e37af43dc2227378d9
Saulo <sauloproetti@gmail.com>
2026-08-23 19:14:20 -0300

    fix: cores

--- gamekit.sh
+++ gamekit.sh
@@ -34,14 +34,14 @@ readonly KIT_AUTO_NOTA='As escolhas de componentes são salvas por sessão.'
 
 # Cores ANSI - respeita NO_COLOR
 if [[ -t 1 && -z "${NO_COLOR:-}" ]]; then
-    readonly COLOR_RESET='\033[0m'
-    readonly COLOR_BOLD='\033[1m'
-    readonly COLOR_GREEN='\033[0;32m'
-    readonly COLOR_RED='\033[0;31m'
-    readonly COLOR_YELLOW='\033[0;33m'
-    readonly COLOR_BLUE='\033[0;34m'
-    readonly COLOR_MAGENTA='\033[0;35m'
-    readonly COLOR_CYAN='\033[0;36m'
+    readonly COLOR_RESET=$'\033[0m'
+    readonly COLOR_BOLD=$'\033[1m'
+    readonly COLOR_GREEN=$'\033[0;32m'
+    readonly COLOR_RED=$'\033[0;31m'
+    readonly COLOR_YELLOW=$'\033[0;33m'
+    readonly COLOR_BLUE=$'\033[0;34m'
+    readonly COLOR_MAGENTA=$'\033[0;35m'
+    readonly COLOR_CYAN=$'\033[0;36m'
 else
     readonly COLOR_RESET=''
     readonly COLOR_BOLD=''
